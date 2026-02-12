4203a1e5503bef2040b863f40c2365b263e9cd89
Paul Senatillaka <paulsena@gmail.com>
2026-02-12 18:49:28 -0500

    - Commit basic auto code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/BasicAutonomousNoEncodersBlue.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode.autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+@Autonomous(name = "Basic Autonomous - Blue (No Encoders)", group = "1 - Team Code")
+public class BasicAutonomousNoEncodersBlue extends BaseAutonomousNoEncoders
+{
+    @Override
+    protected double getStrafePower() { return -DRIVE_POWER; }
+}
